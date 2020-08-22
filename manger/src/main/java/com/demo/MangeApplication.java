91e2149ad7e5f9082883a10da268b6bb592c5099
karl <xihb103@163.com>
2020-08-22 09:41:19 +0800

    创建一个初始化项目

---
+++ manger/src/main/java/com/demo/MangeApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangeApplication.class, args);
+	}
+
+}
